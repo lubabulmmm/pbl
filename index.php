b95b796ec300ecd988eca8798f094f04ef209848
lubabulmmm <lubabul.ilmi16@gmail.com>
2023-12-08 18:21:41 +0700

    update

--- index.php
+++ index.php
@@ -21,11 +21,11 @@
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="max-w-2xl mb-4 text-4xl dark:text-gray-900 text-gray-500  font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl ">Vokasi Universitas Brawijaya</h1>
                 <p class="max-w-2xl mb-6 font-light dark:text-gray-500 text-gray-500 lg:mb-8 md:text-lg lg:text-xl text-gray-900">Pendidikan vokasi merupakan pendidikan yang mengarahkan mahasiswa untuk mengembangkan keahlian terapan, beradaptasi pada bidang pekerjaan tertentu dan dapat menciptakan peluang kerja Pendidikan vokasi menganut sistem terbuka (multi-entry-exit system) dan multimakna berorientasi pada pembudayaan, pemberdayaan, pembentukan watak, dan kepribadian, serta berbagai kecakapan hidup life skill. </p>
-                  <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-900 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                  <a href="./showcase.php" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-900 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                     Lihat ShowCase
                     <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
-                <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-500 border border-amber-600 rounded-lg hover:bg-amber-600 bg-amber-500 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                <a href="./login.php" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-500 border border-amber-600 rounded-lg hover:bg-amber-600 bg-amber-500 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                     Login
                 </a> 
             </div>
@@ -43,10 +43,10 @@
             <div class="mt-4 md:mt-0">
                 <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">Tentang PBL</h2>
                 <p class="mb-6 font-light text-gray-500 md:text-lg">Pembelajaran berbasis proyek adalah metoda pembelajaran yang menggunakan proyek/kegiatan sebagai media. Peserta didik melakukan eksplorasi, penilaian, interpretasi, sintesis, dan informasi untuk menghasilkan berbagai bentuk hasil belajar.</p>
-                <a href="#" class="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
+                <!-- <a href="#" class="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
                     Get started
                     <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </a>
+                </a> -->
             </div>
         </div>
     </section>
