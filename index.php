8dbb8c965233b5963d115ac6a8c4682d20b0d2d1
lubabulmmm <lubabul.ilmi16@gmail.com>
2023-12-08 19:04:25 +0700

    update

--- index.php
+++ index.php
@@ -180,40 +180,48 @@
       </section>
 
 
-<!-- carsoul -->
-<section >
+      
+      <section >
 <div class="py-8 px-4 mx-auto max-w-screen-md sm:py-16 lg:px-6">
 <div class="max-w-screen-full mb-8 lg:mb-16">
     <h2 class = "mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 ">ShowCase PBL</h2>
 </div>
-<div id="controls-carousel" class="relative w-full" data-carousel="static">
+<div id="default-carousel" class="relative w-full" data-carousel="slide">
     <!-- Carousel wrapper -->
-    <div class="relative h-60 overflow-hidden rounded-lg md:h-96">
+    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
          <!-- Item 1 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="./assets/img/poster4.png" class="absolute block h-96 md:h-96 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <img src="./assets/img/poster4.png" class="absolute block h-96 md:h-96 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
         <!-- Item 2 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
-            <img src="./assets/img/bnnr-13.png" class="absolute block h-96 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+        <img src="./assets/img/bnnr-13.png" class="absolute block h-96 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
         <!-- Item 3 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="./assets/img/bnnr-14.png" class="absolute block h-96 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <img src="./assets/img/bnnr-14.png" class="absolute block h-96 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
         <!-- Item 4 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="./assets/img/bnnr-12.png" class="absolute block h-96 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <img src="./assets/img/bnnr-12.png" class="absolute block h-96 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
         <!-- Item 5 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="./assets/img/bnnr-13.png" class="absolute block h-96 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <img src="./assets/img/bnnr-13.png" class="absolute block h-96 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
+    </div>
+    <!-- Slider indicators -->
+    <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
     </div>
     <!-- Slider controls -->
     <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
         <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-gray-700 dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
             </svg>
             <span class="sr-only">Previous</span>
@@ -221,7 +229,7 @@
     </button>
     <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
         <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-gray-700 dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
             </svg>
             <span class="sr-only">Next</span>
@@ -229,7 +237,9 @@
     </button>
 </div>
 </div>
-</section>
+      </section>
+
+
 
 
    
