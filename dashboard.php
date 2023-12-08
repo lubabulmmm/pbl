70d7697b1a7b660d87727a689407f923e5ec419c
Ibrahim Irsad <ibrairsyad1712@gmail.com>
2023-12-08 14:17:06 +0700

    change halo

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dashboard | PBL Vokasi</title>
+  <title>Dashboard Mahasiswa | PBL Vokasi</title>
   <?php include("./includes/head.php") ?>
 </head>
 <body class="bg-gray-50 ">
