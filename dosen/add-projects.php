0143453b3e7bc310c61b3d0fb87d8722148284b9
lubabulmmm <lubabul.ilmi16@gmail.com>
2023-12-09 09:37:00 +0700

    update

--- dosen/add-projects.php
+++ dosen/add-projects.php
@@ -33,7 +33,7 @@
                   <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
                   </svg>
-                  <span class="ms-1 text-lg font-medium text-blue-500 md:ms-2">Tambah Proyek</span>
+                  <span class="ms-1 text-lg font-medium text-blue-500 md:ms-2">Tambah Judul Proyek</span>
                 </div>
               </li>
             </ol>
