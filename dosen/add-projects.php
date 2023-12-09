5335a81e8bcd11e0b2d6c9377b7c243a817a9948
lubabulmmm <lubabul.ilmi16@gmail.com>
2023-12-09 09:44:36 +0700

    update

--- dosen/add-projects.php
+++ dosen/add-projects.php
@@ -42,7 +42,7 @@
 
           <section class="bg-gray-50">
             <div class="py-8 px-4 mx-auto max-w-2xl lg:py-13">
-                <h2 class="mb-4 text-xl font-bold text-gray-900">Tambah Proyek</h2>
+                <h2 class="mb-4 text-xl font-bold text-gray-900">Tambah Judul Proyek</h2>
                 <form action="#">
                     <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                         <div class="sm:col-span-2">
@@ -50,19 +50,19 @@
                             <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Tambah Nama Proyek.." required="">
                         </div>
                         <div>
-                            <label for="category" class="block mb-2 text-sm font-medium text-gray-900">Pilih Dosen</label>
+                            <label for="category" class="block mb-2 text-sm font-medium text-gray-900">Pilih Jurusan</label>
                             <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
-                                <option selected="">Pilih Dosen</option>
-                                <option value="TV">daniadhitya@gmail.com</option>
-                                <option value="PC">anaknyakeren17@outlook.com</option>
-                                <option value="GA">emangboleh88@yahoo.com</option>
-                                <option value="PH">tengokkakilahmail@gmail.com</option>
+                                <option selected="">Pilih Jurusan</option>
+                                <option value="TI">Teknologi Informasi</option>
+                                <option value="DG">Design Grafis</option>
+                                <option value="PH">Perhotelan</option>
+                                <option value="PK">Perbankan</option>
                             </select>
                         </div> 
-                        <div>
+                        <!-- <div>
                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Nama Proyek</label>
                             <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Masukkan Minggu" required="">
-                        </div> 
+                        </div>  -->
                         <div class="sm:col-span-2">
                             <label for="description" class="block mb-2 text-sm font-medium text-gray-900">Tambah Deskripsi</label>
                             <textarea id="description" rows="8" class="block p-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Tambah Deskripsi Proyek.."></textarea>
