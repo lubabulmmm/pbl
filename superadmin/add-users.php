e454cd26993a002081ae9d0d29fd6cbedad12df3
ifnfadillah <143673321+ifnfadillah@users.noreply.github.com>
2024-01-25 20:50:42 +0700

    aaaaaa

--- superadmin/add-users.php
+++ superadmin/add-users.php
@@ -20,9 +20,9 @@ require '../query/query.php';
 if (isset($_POST["submit"])) {
 
   if (add_user($_POST) > 0) {
-    header("Location: users.php?info=success");
+    header("Location: users.php");
   } else {
-    header("Location: users.php?info=failed");
+    header("Location: users.php");
   }
 }
 
