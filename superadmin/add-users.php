e194f83d99de71befca736f0ce32a44adad1afb1
mirsyadan <mohirsyad09@gmail.com>
2024-01-27 23:01:04 +0700

    update label nama

--- superadmin/add-users.php
+++ superadmin/add-users.php
@@ -117,8 +117,8 @@ if (isset($_POST["submit"])) {
             <form action="" method="post">
               <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                 <div class="sm:col-span-2">
-                  <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Nama Mahasiswa</label>
-                  <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Maukkan Nama" required="">
+                  <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Nama</label>
+                  <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Masukkan Nama" required="">
                 </div>
                 <div>
                   <label for="nim" class="block mb-2 text-sm font-medium text-gray-900">NIM</label>
