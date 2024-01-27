16d960e4664adfa3056f6163950d3db95ea745c0
mirsyadan <mohirsyad09@gmail.com>
2024-01-27 23:18:36 +0700

    update ketua kelompok

--- superadmin/superadmin.php
+++ superadmin/superadmin.php
@@ -177,7 +177,7 @@ $list_bunchOnCurrentPage = array_slice($list_bunch, $offset, $itemsPerPage);
                     <th scope="col" class="lg:px-4 lg:py-3 px-2 py-3">No</th>
                     <th scope="col" class="lg:px-4 lg:py-3 px-2 py-3">Judul Proyek</th>
                     <th scope="col" class="lg:px-4 lg:py-3 px-2 py-3">Kelompok</th>
-                    <th scope="col" class="lg:px-4 lg:py-3 px-2 py-3">Nama Ketua</th>
+                    <th scope="col" class="lg:px-4 lg:py-3 px-2 py-3">Ketua Kelompok</th>
                     <th scope="col" class="lg:px-4 lg:py-3 px-2 py-3">Nama Dosen PIC</th>
                     <th scope="col" class="lg:px-4 lg:py-3 px-2 py-3">
                       <span class="sr-only">Action</span>
