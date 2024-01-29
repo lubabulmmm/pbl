7278501ed82d42bcbcf8c836611230dd353d0e61
mirsyadan <mohirsyad09@gmail.com>
2024-01-29 22:16:51 +0700

    update daftar kelompok

--- superadmin/groups-and-projects/details-projects.php
+++ superadmin/groups-and-projects/details-projects.php
@@ -83,7 +83,7 @@ $sum_num = mysqli_num_rows($sum_bunch);
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7.708 2.292.706-.706A2 2 0 0 1 9.828 1h6.239A.97.97 0 0 1 17 2v12a.97.97 0 0 1-.933 1H15M6 5v4a1 1 0 0 1-1 1H1m11-4v12a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V9.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 5h5.239A.97.97 0 0 1 12 6Z" />
                 </svg>
 
-                Detail Kelompok
+                Daftar Kelompok
               </button>
             </a>
           </div>
