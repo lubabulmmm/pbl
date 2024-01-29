412a6b374bd6c65bebed39ea2d0215e7c9f6d27c
mirsyadan <mohirsyad09@gmail.com>
2024-01-29 21:17:26 +0700

    update teks bar kumpulan

--- superadmin/groups-and-projects/projects-list.php
+++ superadmin/groups-and-projects/projects-list.php
@@ -80,7 +80,7 @@ $projectsOnCurrentPage = array_slice($projects, $offset, $itemsPerPage);
                   <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                     <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
                   </svg>
-                  Kumpulan Daftar Proyek
+                  Daftar Proyek
                 </a>
               </li>
             </ol>
@@ -132,7 +132,7 @@ $projectsOnCurrentPage = array_slice($projects, $offset, $itemsPerPage);
             <div class="bg-blue-900 flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
 
               <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                <h2 class="text-xl font-semibold text-white">Daftar Proyek</h2>
+                <h2 class="text-xl font-semibold text-white">Kumpulan Daftar Proyek</h2>
               </div>
 
               <div class="w-full md:w-1/2">
