97818e95b1803ea04f4cbc60e0ae7d9aeadbe49b
Ibrahim Irsad <63491368+Ibrairsyad17@users.noreply.github.com>
2024-01-29 20:10:22 +0700

    user dashboard

--- user/dashboard.php
+++ user/dashboard.php
@@ -73,35 +73,7 @@ $projects = execThis("SELECT id_proyek, nama_proyek, deskripsi_proyek, nama_user
 
       <div class="py-8 max-w-screen-xl lg:py-6 lg:px-6">
         <div class="grid gap-8 lg:grid-cols-2">
-          <article class="p-6 rounded-xl border shadow-sm bg-white border-amber-400 hover:shadow-lg duration-300 h-72 flex box-border flex-col justify-between ease-in-out">
-            <div class="flex justify-between items-center mb-5 text-gray-500">
-              <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded bg-amber-200 text-amber-800">
-                <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
-                </svg>
-                Teknologi Informasi
-              </span>
-              <span class="text-sm"></span>
-            </div>
-            <h2 class="mb-2 text-xl font-bold tracking-tight text-gray-900"><a href="#">Sistem Informasi E-Complain</a></h2>
-            <p class="mb-5 font-light text-sm text-gray-500">
-              Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.
-            </p>
-            <div class="flex justify-between items-center">
-              <div class="flex items-center space-x-4">
-                <img class="w-7 h-7 rounded-full" src="./assets/img/flora.jpg" alt="" />
-                <span class="font-medium">
-                  Flora Shafiq Riyadi
-                </span>
-              </div>
-              <a href="" class="inline-flex items-center font-medium text-amber-500 hover:underline">
-                Kunjungi Proyek
-                <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                </svg>
-              </a>
-            </div>
-          </article>
+          <?php include("../content/user-project.php") ?>
         </div>
       </div>
 
