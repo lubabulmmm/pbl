d3ad12a9ee4e7c021c8a6110fa23adfdee038a06
lubabulmmm <lubabul.ilmi16@gmail.com>
2024-01-29 11:52:05 +0700

    limit live search user in superadmin

--- superadmin/live-search/live_search.php
+++ superadmin/live-search/live_search.php
@@ -1,10 +1,20 @@
 <?php
+// live_searchadmin.php
+
 require '../../query/query.php';
 
-if (isset($_POST["keyword"])) {
-    $keyword = $_POST["keyword"];
-    $users = execThis("SELECT * FROM user WHERE level = 'user' AND (nama_user LIKE '%$keyword%' OR email LIKE '%$keyword%' OR id LIKE '%$keyword%')");
-    echo json_encode($users);
-} else {
-    echo json_encode([]);
+$itemsPerPage = 5; // Set the desired limit
+
+if (isset($_POST['keyword'])) {
+  $keyword = $_POST['keyword'];
+
+  $users = execThis("SELECT * FROM user WHERE level = 'user' AND (nama_user LIKE '%$keyword%' OR email LIKE '%$keyword%' OR id LIKE '%$keyword%')");
+
+  // Limit the results to $itemsPerPage
+  $users = array_slice($users, 0, $itemsPerPage);
+
+  // Output JSON
+  header('Content-Type: application/json');
+  echo json_encode($users);
 }
+?>
