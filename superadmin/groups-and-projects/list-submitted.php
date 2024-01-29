7e21d517c5d06f4cacd930688a374bc077979d79
ifnfadillah <143673321+ifnfadillah@users.noreply.github.com>
2024-01-29 19:19:20 +0700

    update span -daftar kel

--- superadmin/groups-and-projects/list-submitted.php
+++ superadmin/groups-and-projects/list-submitted.php
@@ -67,7 +67,7 @@ if (isset($_SESSION["level"])) {
                 <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                 </svg>
-                <span class="ms-1 lg:text-lg text-xs md:text-md font-medium text-blue-800 md:ms-2">Data Pengumpulan Kelompok</span>
+                <span class="ms-1 lg:text-lg text-xs md:text-md font-medium text-blue-800 md:ms-2">Daftar Kelompok</span>
               </div>
             </li>
           </ol>
