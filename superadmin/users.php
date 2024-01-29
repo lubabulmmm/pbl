3979b4ddd0afeecb5e465bc256f8be05858fa642
mirsyadan <mohirsyad09@gmail.com>
2024-01-29 21:12:58 +0700

    update teks info notif mahasiswa

--- superadmin/users.php
+++ superadmin/users.php
@@ -86,7 +86,7 @@ $usersOnCurrentPage = array_slice($users, $offset, $itemsPerPage);
                 </svg>
                 <span class="sr-only">Info</span>
                 <div class="ms-3 text-sm font-medium">
-                  Data berhasil di hapus!
+                  Selamat, data berhasil di hapus!
                 </div>
                 <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8" data-dismiss-target="#alert-3" aria-label="Close">
                   <span class="sr-only">Close</span>
@@ -105,7 +105,7 @@ $usersOnCurrentPage = array_slice($users, $offset, $itemsPerPage);
                 </svg>
                 <span class="sr-only">Info</span>
                 <div class="ms-3 text-sm font-medium">
-                  Data gagal di hapus!
+                  Mohon Maaf, data gagal di hapus!
                 </div>
                 <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8" data-dismiss-target="#alert-3" aria-label="Close">
                   <span class="sr-only">Close</span>
