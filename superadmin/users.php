35f00ba64d94fca703a7777389b60e897f422c4d
mirsyadan <mohirsyad09@gmail.com>
2024-01-29 20:04:22 +0700

    UPDATE TEKS

--- superadmin/users.php
+++ superadmin/users.php
@@ -136,7 +136,7 @@ $usersOnCurrentPage = array_slice($users, $offset, $itemsPerPage);
                         <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
                       </svg>
                     </div>
-                    <input type="text" name="keyword" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-blue-900 block w-full pl-10 p-2 mr-3" placeholder="Cari mahasiswa">
+                    <input type="text" name="keyword" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-blue-900 block w-full pl-10 p-2 mr-3" placeholder="Cari Mahasiswa">
                   </div>
 
                   <button type="submit" class="flex items-center justify-center text-white bg-amber-500 hover:bg-amber-600 focus:ring-4 focus:ring-amber-300 font-medium rounded-lg text-sm px-4 py-2 ml-4">
