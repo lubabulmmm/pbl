b91cc4019d38982aa898756aad0f56ae9028342d
mirsyadan <mohirsyad09@gmail.com>
2024-01-30 15:10:14 +0700

    update add user n dosen n edit proyek

--- superadmin/add-users.php
+++ superadmin/add-users.php
@@ -76,20 +76,20 @@ if (isset($_POST["submit"])) {
             <form action="" method="post">
               <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                 <div class="sm:col-span-2">
-                  <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Nama Mahasiswa</label>
-                  <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Masukkan Nama Mahasiswa.." required="">
+                  <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Nama</label>
+                  <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Masukkan Nama" required="">
                 </div>
                 <div>
                   <label for="nim" class="block mb-2 text-sm font-medium text-gray-900">NIM</label>
-                  <input type="text" name="nim" id="nim" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Masukkan NIM.." required="">
+                  <input type="text" name="nim" id="nim" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Masukkan NIM" required="">
                 </div>
                 <div class="sm:col-span-2">
-                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email Mahasiswa</label>
-                  <input type="text" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Masukkan Email Mahasiswa.." required="">
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
+                  <input type="text" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Masukkan Email" required="">
                 </div>
                 <div class="sm:col-span-2">
                   <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                  <input type="password" name="password" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Masukkan Password.." required="">
+                  <input type="password" name="password" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Masukkan Password" required="">
                 </div>
               </div>
               <button type="submit" name="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-800 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-blue-400">
