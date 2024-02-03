0afe0aad2e99e792434be534a60d8d7f2f0dac64
lubabulmmm <lubabul.ilmi16@gmail.com>
2024-02-03 08:39:12 +0700

    navbarshowcase

--- user/includes/navbarshowcase.php
+++ user/includes/navbarshowcase.php
@@ -31,56 +31,69 @@
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="../index.php" class="rounded-md px-3 py-2 text-sm font-medium hover:text-amber-500 z-50" aria-current="page">Beranda</a>
-            <a href="./#tentang" class=" hover:text-amber-500 z-50 rounded-md px-3 py-2 text-sm font-medium">Tentang</a>
-            <a href="./showcase.php" class="bg-amber-500 text-white rounded-md px-3 py-2 text-sm font-medium">Showcase PBL</a>
+            <a href="../index.php" class="rounded-md px-3 py-2 text-md font-medium hover:text-amber-500 z-50" aria-current="page">Beranda</a>
+            <!-- <a href="../index.php/#tentang" class=" hover:text-amber-500 z-50 rounded-md px-3 py-2 text-md font-medium">Tentang</a> -->
+            <a href="./user/showcase.php" class="bg-amber-500 text-white rounded-md px-3 py-2 text-md font-medium">Showcase PBL</a>
           </div>
         </div>
       </div>
-      <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-        <span class="text-sm mx-2">Hi, Jake Pierce</span>
-
+      
         <!-- Profile dropdown -->
-        <div class="relative ml-3 pfpBtn duration-300 ease-in-out">
+        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        <!-- HERE -->
+        <?php if(isset($_SESSION["login"])){ ?>
+          <span class="text-sm mx-2">Hi, <?= $_SESSION["nama_user"] ?></span>
+
+          <!-- Profile dropdown -->
           <div>
-            <button type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-              <span class="absolute -inset-1.5"></span>
+            <button type="button" class="flex text-sm bg-amber-500 rounded-full focus:ring-4 focus:ring-amber-300" aria-expanded="false" data-dropdown-toggle="dropdown-user">
               <span class="sr-only">Open user menu</span>
-              <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+              <img class="w-9 h-9 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="user photo">
             </button>
           </div>
-
-          <!--
-            Dropdown menu, show/hide based on menu state.
-
-            Entering: "transition ease-out duration-100"
-              From: "transform opacity-0 scale-95"
-              To: "transform opacity-100 scale-100"
-            Leaving: "transition ease-in duration-75"
-              From: "transform opacity-100 scale-100"
-              To: "transform opacity-0 scale-95"
-          -->
-          <div class="hidden pfpMenu absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none duration-300 ease-in-out" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-            <!-- Active: "bg-gray-100", Not Active: "" -->
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-amber-500 hover:text-white" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-amber-500 hover:text-white" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+          <div class="z-50 hidden my-4 text-base text-gray-900 list-none divide-y rounded shadow-lg bg-white divide-gray-300" id="dropdown-user">
+            <div class="px-4 py-3" role="none">
+              <p class="text-sm text-gray-900" role="none"><?= $_SESSION["nama_user"] ?>
+              </p>
+              <p class="text-sm font-medium text-gray-900 truncate" role="none"><?= $_SESSION["email"] ?>
+              </p>
+            </div>
+            <ul class="py-1" role="none">
+              <li>
+                <a href="/PBL/user/dashboard.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 " role="menuitem">Dashboard</a>
+              </li>
+              <li>
+                <a href="/PBL/user/logout.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 " role="menuitem">Keluar</a>
+              </li>
+            </ul>
           </div>
-        </div>
+
+            <!-- DROP DOWN -->
+            <div class="hidden pfpMenu absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-violet-200 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none duration-300 ease-in-out" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+              <!-- Active: "bg-gray-100", Not Active: "" -->
+              <a href="./dashboard.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-violet-500 hover:text-white" role="menuitem" tabindex="-1" id="user-menu-item-0">Dashboard</a>
+              <a href="./logout.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-violet-500 hover:text-white" role="menuitem" tabindex="-1" id="user-menu-item-2">Logout</a>
+            </div>
+          </div>
+          <a href="./user/login.php" class="hidden inline-flex items-center px-4 py-2 text-md font-medium text-center text-gray-800 rounded-lg hover:bg-amber-500 focus:ring-4 focus:outline-none ">Login</a>
+        <?php }else{ ?>
+          <a href="./user/login.php" class="inline-flex items-center px-4 py-2 text-md font-medium text-center text-gray-800  rounded-lg hover:bg-amber-500 focus:ring-4 focus:outline-none ">Login</a>
+        <?php } ?>
       </div>
     </div>
   </div>
+    
 
-  <!-- Mobile menu, show/hide based on menu state. -->
-  <div class="sm:hidden navHam1 hidden duration-300 ease-in-out" id="mobile-menu">
+   <!-- Mobile menu, show/hide based on menu state. -->
+   <div class="sm:hidden navHam hidden duration-300 ease-in-out" id="mobile-menu">
     <div class="space-y-1 px-2 pb-3 pt-2 duration-300 ease-in-out">
       <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-      <a href="#" class=" block rounded-md px-3 py-2 hover:bg-amber-500 hover:text-white text-base font-medium" aria-current="page">Home</a>
-      <a href="#" class="hover:bg-amber-500 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Tentang</a>
-      <a href="#" class="hover:bg-amber-500 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Showcase PBL</a>
+      <a href="../index.php" class="rounded-md px-3 py-2 text-md font-medium hover:text-amber-500 z-50" aria-current="page">Beranda</a>
+            <!-- <a href="../index.php/#tentang" class=" hover:text-amber-500 z-50 rounded-md px-3 py-2 text-md font-medium">Tentang</a> -->
+            <a href="./user/showcase.php" class="bg-amber-500 text-white rounded-md px-3 py-2 text-md font-medium">Showcase PBL</a>
     </div>
   </div>
 </nav>
-
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
 <script src="../js/script.js"></script>
