0c2eb07a3c6e7907349a923a05e325252fecc297
mirsyadan <mohirsyad09@gmail.com>
2024-02-07 22:53:57 +0700

    profil

--- user/profile.php
+++ user/profile.php
@@ -17,6 +17,7 @@ if (isset($_SESSION["level"])) {
 
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,7 +25,38 @@ if (isset($_SESSION["level"])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Profil Kamu | PBL Vokasi</title>
-  <?php include("./includes/head.php") ?>
+  <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+  <link rel="shortcut icon" href="/PBL/assets/img/favicon/favicon.ico" type="image/x-icon">
+  <link rel="apple-touch-icon" sizes="180x180" href="/PBL/assets/img/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/PBL/assets/img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/PBL/assets/img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800&display=swap');
+
+    body {
+      font-family: 'Inter', sans-serif;
+    }
+
+    html {
+      scroll-behavior: smooth;
+    }
+
+    .os:hover {
+      background-color: #FFAA2A;
+    }
+
+    .od:hover {
+      background-color: #53659F;
+    }
+
+    @media screen and (max-width: 600px) {
+      .name {
+        display: none;
+      }
+    }
+  </style>
 </head>
 
 <body class="">
@@ -45,7 +77,7 @@ if (isset($_SESSION["level"])) {
         <nav class="flex my-7" aria-label="Breadcrumb">
           <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
             <li class="inline-flex items-center">
-              <a href="./dashboard.php" class="inline-flex items-center text-lg font-medium text-gray-700 hover:text-amber-500">
+              <a href="./dashadmin.php" class="inline-flex items-center text-lg font-medium text-gray-700 hover:text-blue-500">
                 <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
                 </svg>
@@ -57,7 +89,7 @@ if (isset($_SESSION["level"])) {
                 <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                 </svg>
-                <span class="ms-1 text-lg font-medium text-amber-500 md:ms-2">Profil</span>
+                <span class="ms-1 text-lg font-medium text-blue-500 md:ms-2">Profil</span>
               </div>
             </li>
           </ol>
@@ -65,77 +97,24 @@ if (isset($_SESSION["level"])) {
 
         <form>
           <div class="space-y-12">
-            <div class="border-b border-gray-900/10 pb-12">
+            <div class="border-b border-gray-900/10 pb-5">
               <h2 class="text-xl font-semibold leading-7 text-gray-900">Profil Kamu</h2>
 
-
               <div class="mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div class="col-span-full my-5">
-                  <label for="photo" class="block text-sm font-medium leading-6 text-gray-900">Foto</label>
+                <div class="col-span-full">
+                  <label for="photo" class="block text-sm font-medium leading-6 text-gray-900">Photo</label>
                   <div class="mt-2 flex items-center gap-x-3">
-                    <svg class="h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                      <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
-                    </svg>
-                    <button type="button" class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Ubah Foto</button>
-                  </div>
-                </div>
+                    <img src="../assets/img/profile.png" class="h-20 w-20" alt="">
+                    <div class="w-2/12">
 
-
-
-                <div class="sm:col-span-4">
-                  <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Nama</label>
-                  <div class="mt-2">
-                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-amber-600 sm:max-w-md">
-                    <div class="name px-3 text-sm block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900"> <?= $_SESSION['nama_user'] ?></div>                      
+                      <input type="file" name="" id="" class="text-sm file:bg-white file:border-blue-700 file:text-blue-700 file:text-sm file:border file:py-1 file:px-3 file:rounded-full">
                     </div>
+                    <button class="flex items-center flex-wrap ml-auto bg-amber-500 text-white rounded-xl mx-3 px-2 py-1 text-sm ">Ubah</button>
                   </div>
+
                 </div>
-
-                <div class="sm:col-span-4">
-                  <label for="nim" class="block text-sm font-medium leading-6 text-gray-900">NIM</label>
-                  <div class="mt-2">  
-                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-amber-600 sm:max-w-md">
-                    <div class="name px-3 text-sm block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900"><?= $_SESSION['id'] ?></div>                      
-                      <!-- <input type="password" name="password" id="password" autocomplete="password" class="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Masukkan kata sandi lama"> -->
-                    </div>
-                  </div>
-                </div>
-
-                <div class="sm:col-span-4">
-                  <label for="Email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
-                  <div class="mt-2">
-                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-amber-600 sm:max-w-md">
-                    <div class="name px-3 text-sm block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900"><?= $_SESSION['email'] ?></div>                      
-                      <!-- <input type="password" name="password" id="password" autocomplete="password" class="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Masukkan kata sandi baru"> -->
-                    </div>
-                  </div>
-                </div>
-
-
-
               </div>
             </div>
-
-
-
-           
-          <form>
-          <!-- <div class="flex items-center justify-start gap-x-5">
-              <button type="submit" class="rounded-md bg-blue-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 ">Simpan</button>
-              <button type="submit" class="rounded-md bg-red-500 px-5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-400 ">Batal</button>
-            </div> -->
-            <button type="submit" name="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-800 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-blue-400">
-                Simpan
-              </button>
-              </button>
-              <button type="cancle" name="cancle" class="inline-flex items-center px-7 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-amber-500 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-amber-400">
-                Batal
-              </button>
-              <a href="./dashboard.php" type="button" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-red-500 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-red-400">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4" />
-                Kembali
-              </a>
-          </form>
         </form>
 
       </div>
@@ -145,7 +124,6 @@ if (isset($_SESSION["level"])) {
 
 
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
 </body>
 
 </html>
