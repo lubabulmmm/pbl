acf0d5ef06a90c0146a5e7a3ce2aca05c133495f
ifnfadillah <143673321+ifnfadillah@users.noreply.github.com>
2024-02-11 05:19:41 +0700

    fix dialog delete popup -super

--- superadmin/content/delete-modal.php
+++ superadmin/content/delete-modal.php
@@ -11,7 +11,7 @@
         <svg class="mx-auto mb-4 text-red-600 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
         </svg>
-        <h3 class="mb-5 text-md font-normal text-gray-500 dark:text-gray-400">Apakah kamu yakin menolak <span class="font-medium"><?= $user['nama_user'] ?></span> ?</h3>
+        <h3 class="mb-5 text-md font-normal text-gray-500 dark:text-gray-400">Apakah kamu yakin menghapus  <span class="font-medium"><?= $user['nama_user'] ?></span> ?</h3>
         <a href="./delete-user.php?id=<?= $user['id'] ?>" type="button" class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2 text-center me-2">
           Ya, saya yakin
         </a>
