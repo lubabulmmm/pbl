3f9148c0031e795f1cb2b51b74dea77674f308fe
lubabulmmm <lubabul.ilmi16@gmail.com>
2024-02-11 21:14:33 +0700

    buat akun in login

--- superadmin/edits-users.php
+++ superadmin/edits-users.php
@@ -53,18 +53,28 @@ if (isset($_POST["submit"])) {
 
       <section class="p-3 sm:p-5">
         <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
-          <nav class="flex my-4" aria-label="Breadcrumb">
+          <nav class="flex my-7" aria-label="Breadcrumb">
             <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
               <li class="inline-flex items-center">
-                <a href="./users.php"
-                  class="inline-flex items-center text-lg font-medium text-gray-700 hover:text-blue-900">
+                <a href="./superadmin.php"
+                  class="inline-flex items-center text-lg font-medium text-gray-700 hover:text-blue-800">
                   <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                     viewBox="0 0 20 20">
                     <path
                       d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
                   </svg>
-                  Mahasiswa
+                  Daftar Proyek
                 </a>
+              </li>
+              <li aria-current="page">
+                <div class="flex items-center">
+                  <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="m1 9 4-4-4-4" />
+                  </svg>
+                  <span class="ms-1 text-lg font-medium text-blue-800 md:ms-2">Edit User</span>
+                </div>
               </li>
             </ol>
           </nav>
--- user/profile.php
+++ user/profile.php
@@ -77,17 +77,22 @@ if (isset($_SESSION["level"])) {
         <nav class="flex my-7" aria-label="Breadcrumb">
           <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
             <li class="inline-flex items-center">
-              <a href="./dashadmin.php" class="inline-flex items-center text-lg font-medium text-gray-700 hover:text-blue-500">
-                <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+              <a href="./dashboard.php"
+                class=" inline-flex items-center text-lg font-medium text-gray-700 hover:text-blue-500">
+                <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                  viewBox="0 0 20 20">
+                  <path
+                    d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
                 </svg>
                 Beranda
               </a>
             </li>
             <li aria-current="page">
               <div class="flex items-center">
-                <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="m1 9 4-4-4-4" />
                 </svg>
                 <span class="ms-1 text-lg font-medium text-blue-500 md:ms-2">Profil</span>
               </div>
@@ -107,9 +112,11 @@ if (isset($_SESSION["level"])) {
                     <img src="../assets/img/profile.png" class="h-20 w-20" alt="">
                     <div class="w-2/12">
 
-                      <input type="file" name="" id="" class="text-sm file:bg-white file:border-blue-700 file:text-blue-700 file:text-sm file:border file:py-1 file:px-3 file:rounded-full">
+                      <input type="file" name="" id=""
+                        class="text-sm file:bg-white file:border-blue-700 file:text-blue-700 file:text-sm file:border file:py-1 file:px-3 file:rounded-full">
                     </div>
-                    <button class="flex items-center flex-wrap ml-auto bg-amber-500 text-white rounded-xl mx-3 px-2 py-1 text-sm ">Ubah</button>
+                    <button
+                      class="flex items-center flex-wrap ml-auto bg-amber-500 text-white rounded-xl mx-3 px-2 py-1 text-sm ">Ubah</button>
                   </div>
 
                 </div>
